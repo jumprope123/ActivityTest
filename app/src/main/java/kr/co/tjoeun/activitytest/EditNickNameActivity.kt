f75a3a7a6224abcaf48df84037ad41190d6ae1b7
JiHwan Shin <jumprope123@naver.com>
2020-09-19 10:52:42 +0900

    입력한 닉네임을 가지고 메인으로 복귀

--- app/src/main/java/kr/co/tjoeun/activitytest/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/activitytest/EditNickNameActivity.kt
@@ -1,11 +1,34 @@
 package kr.co.tjoeun.activitytest
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+    
+    okBtn.setOnClickListener{
+        //입력한 닉네임 받아오기
+        
+        val inputNickName = nickNameEdt.text.toString()
+        
+        //입력값을 담기위한 용도의 Intent 생성
+        val resultIntent = Intent()
+        
+        //입력값을 resultIntent에 첨부
+        resultIntent.putExtra("nick",inputNickName)
+
+        //이 화면의 최종 결과를 설정
+        setResult(Activity.RESULT_OK,resultIntent)
+
+        // 메인화면으로 복귀
+        finish()
     }
+    }
+    
+    
 }
