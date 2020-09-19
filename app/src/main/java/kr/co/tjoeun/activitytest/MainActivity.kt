74d6d4a75330b79e2b70d2066ec65ec2c4bac827
JiHwan Shin <jumprope123@naver.com>
2020-09-19 10:44:36 +0900

    닉네임 화면으로 결과 받으러 이동하기

--- app/src/main/java/kr/co/tjoeun/activitytest/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/activitytest/MainActivity.kt
@@ -7,6 +7,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //1000이라는 숫자만 봐서는 => 닉네임을 받는 요청이다 라는 사실 인지 힘듬.
+    val REQUEST_FOR_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +27,17 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("message",inputMessage)
             startActivity(myIntent)
             //수정
+        }
+
+        //닉네임 변경 버튼이 눌리면
+        changeNickNameBtn.setOnClickListener{
+            //닉네임 입력 화면으로 이동 (입력 결과를 받으러 가는것임)
+
+            val myIntent = Intent(this, EditNickNameActivity::class.java)
+
+            //(닉네임입력=>1000 숫자로 대신 표기)결과를 받으러 이동한다고 별도로 명시
+            //1000대신 => 멤버변수로 만들어둔 REQUEST_FOR_NICKNAME를 활용, 가독성 향상
+            startActivityForResult(myIntent,REQUEST_FOR_NICKNAME)
         }
     }
 }
